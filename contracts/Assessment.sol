5f31193ea9e57aa4e09916b374789966c307d4b0
jamesjme <152799512+jamesjme@users.noreply.github.com>
2023-12-20 19:56:03 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,25 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
 //import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    mapping(address => bool) public frozenAccounts;
+    address public nominee;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event NomineeAdded(address nominee);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
+    modifier notFrozen() {
+        require(!frozenAccounts[msg.sender], "Your account is frozen");
+        _;
+    }
+
     function getBalance() public view returns(uint256){
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
+    function deposit(uint256 _amount) public payable notFrozen {
         uint _previousBalance = balance;
 
         // make sure this is the owner
@@ -38,7 +44,7 @@ contract Assessment {
     // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
+    function withdraw(uint256 _withdrawAmount) public notFrozen {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
@@ -56,5 +62,21 @@ contract Assessment {
 
         // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function freezeAccount(address account) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        frozenAccounts[account] = true;
+    }
+
+    function unfreezeAccount(address account) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        frozenAccounts[account] = false;
+    }
+
+    function addNominee(address _nominee) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        nominee = _nominee;
+        emit NomineeAdded(_nominee);
     }
 }
